e6def3fb3cf9eafcb17976ad9cdfd5408054d297
relfto <66303314+relfto@users.noreply.github.com>
2020-06-07 10:29:08 +0530

    Update and rename MainActivity.java to MainActivity.kt

---
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var count=0;
+        val rb=findViewById<Button>(R.id.bt)
+        val rr=findViewById<TextView>(R.id.tx)
+        rb.setOnClickListener{
+            count++;
+            rr.text=count.toString();
+    }
+    }
+
+}
